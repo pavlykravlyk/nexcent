070ec35aaa0886e950fe36a216f4585a50782962
pavlykravlyk <kravchuk.pavlo@ukr.net>
2023-06-11 22:49:00 +0200

    add community section

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,17 +1,10 @@
-import {mobileMenuBtn, mobileMenu, menuBurgerIcon, menuCloseIcon} from './refs.js'
+import {mobileMenuBtn, mobileMenu, menuBurgerIcon, menuCloseIcon, bodyEL} from './refs.js'
 
 mobileMenuBtn.addEventListener('click', toggleMobileMenu);
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('is-open');
-    // toggleMenuIcon()
     menuBurgerIcon.classList.toggle('is-hidden');
     menuCloseIcon.classList.toggle('is-hidden');
+    bodyEL.classList.toggle('menu-open');
 }
-
-// function toggleMenuIcon() {
-//     // menuBurgerIcon.style.display = "inline";
-//     // menuCloseIcon.style.display = "none";
-//     menuBurgerIcon.classList.toggle('is-hidden');
-//     menuCloseIcon.classList.toggle('is-hidden');
-// }
--- js/refs.js
+++ js/refs.js
@@ -1,9 +1,9 @@
 const refs = {
     mobileMenuBtn: document.querySelector(".mobile-menu__button"),
     mobileMenu: document.querySelector(".mobile-menu"),
-
     menuBurgerIcon: document.querySelector(".mobile-menu__icon--burger"),
-    menuCloseIcon: document.querySelector(".mobile-menu__icon--close")
+    menuCloseIcon: document.querySelector(".mobile-menu__icon--close"),
+    bodyEL: document.querySelector("body"),
 };
 
-export const {mobileMenuBtn, mobileMenu, menuBurgerIcon, menuCloseIcon} = refs
+export const {mobileMenuBtn, mobileMenu, menuBurgerIcon, menuCloseIcon, bodyEL} = refs
